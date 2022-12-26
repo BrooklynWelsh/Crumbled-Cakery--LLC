ccad912795208d233a8fe7590b60708d134c513a
Brooklyn Welsh <brooklynwelsh95@gmail.com>
2022-12-25 22:38:47 -0500

    Added local miniflare dev server packages/configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const isDev = !!process.env.DEV;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			build: {
+				minify: !isDev,
+			}
+		}
 	}
 };
 
